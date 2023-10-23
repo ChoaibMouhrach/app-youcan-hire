a1ce7ccf38e18989d00904ae1fe07a94103cedb6
PACY2 <>
2023-10-23 01:54:17 +0000

    feat:add /users to admin protected routes

--- src/app/(lobby)/users/page.tsx
+++ src/app/(lobby)/users/page.tsx
@@ -1,8 +1,16 @@
+import { getAuthUser } from "@/lib/auth";
 import { serverClient } from "@/lib/trpc";
+import { Role } from "@prisma/client";
+import { redirect } from "next/navigation";
 
 const Users = async () => {
-  const users = await serverClient.users.index.query();
+  const session = await getAuthUser();
 
+  if (session?.user.role !== Role.ADMIN) {
+    redirect("/403");
+  }
+
+  const users = await serverClient.users.index.query();
   return <div>{JSON.stringify(users)}</div>;
 };
 
